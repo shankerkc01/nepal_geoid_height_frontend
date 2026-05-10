a1d5a3aac4503bea11da9d28d0625535dee8ca0e
shankerkc01 <shankerkc01@gmail.com>
2026-05-10 17:26:25 +0545

    nepal geoid height frontend as github repo

---
+++ script.js
@@ -0,0 +1,71 @@
+// frontend/script.js
+
+const queryButton = document.getElementById("queryButton");
+
+const resultText = document.getElementById("resultText");
+
+
+queryButton.addEventListener("click", async () => {
+
+    const latitude =
+        document.getElementById("latitude").value;
+
+    const longitude =
+        document.getElementById("longitude").value;
+
+
+    // Input validation
+    if (latitude === "" || longitude === "") {
+
+        resultText.innerHTML =
+            "Please enter both latitude and longitude.";
+
+        return;
+    }
+
+
+    // Show loading message
+    resultText.innerHTML =
+        "Retrieving geoid height...";
+
+
+    try {
+
+        // Backend API URL
+        const apiURL =
+            `https://nepal-geoid-heigh-api.onrender.com/geoid_height?lat=${latitude}&lon=${longitude}`;
+
+
+        // Send request
+        const response = await fetch(apiURL);
+
+
+        // Check server response
+        if (!response.ok) {
+
+            throw new Error(
+                "Backend server error"
+            );
+        }
+
+
+        // Parse JSON
+        const data = await response.json();
+
+
+        // Display results
+        resultText.innerHTML = `
+            <strong>Latitude:</strong> ${data.latitude}<br>
+            <strong>Longitude:</strong> ${data.longitude}<br>
+            <strong>Geiod Height:</strong> ${data.geoid_height.toFixed(2)} meters
+        `;
+
+    } catch (error) {
+
+        console.error(error);
+
+        resultText.innerHTML =
+            "Failed to retrieve geoid_height.";
+    }
+
+});
